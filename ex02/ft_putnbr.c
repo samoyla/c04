ae979920bce44005b72ed3f66ea083c48d2475b0
Maryna SAMOILENKO <msamoile@e3r10p4.42.fr>
2021-02-19 15:12:40 +0100

    00-03

--- ex02/ft_putnbr.c
+++ ex02/ft_putnbr.c
@@ -1,32 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msamoile <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/17 15:46:59 by msamoile          #+#    #+#             */
-/*   Updated: 2021/02/18 08:20:42 by msamoile         ###   ########.fr       */
+/*   Created: 2021/02/18 08:18:12 by msamoile          #+#    #+#             */
+/*   Updated: 2021/02/18 10:01:02 by msamoile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-void	ft_putstr(char *str)
-{
-	char *p;
-
-	p = str;
-	while (*p)
-	{
-		write(1, p, 1);
-		p++;
-	}
-}
-
 #include <unistd.h>
 
-int main()
+void ft_putchar(char c)
 {
-	char str[] = "what's the story, morning glory";
-	ft_putstr(str);
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1,"-2147483648", 11);
+	}
+	else if (nb >= 0 && nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+	else if (nb < 0 && nb != -2147483648)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * (-1));
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+
+}
+
+int	main()
+{
+	ft_putnbr(42);
 	return (0);
 }
--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -1,32 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: msamoile <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/17 15:46:59 by msamoile          #+#    #+#             */
-/*   Updated: 2021/02/18 08:20:42 by msamoile         ###   ########.fr       */
+/*   Created: 2021/02/18 09:51:28 by msamoile          #+#    #+#             */
+/*   Updated: 2021/02/18 10:37:39 by msamoile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putstr(char *str)
+int	ft_atoi(char *str);
 {
-	char *p;
-
-	p = str;
-	while (*p)
-	{
-		write(1, p, 1);
-		p++;
-	}
-}
-
-#include <unistd.h>
-
-int main()
-{
-	char str[] = "what's the story, morning glory";
-	ft_putstr(str);
-	return (0);
+	
 }
