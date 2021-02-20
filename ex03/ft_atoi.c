655579e6db52a5fd94970cb6e4b0f261574f96c9
Maryna SAMOILENKO <msamoile@e3r10p4.42.fr>
2021-02-20 16:14:12 +0100

    c04

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,11 +6,42 @@
 /*   By: msamoile <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 09:51:28 by msamoile          #+#    #+#             */
-/*   Updated: 2021/02/18 10:37:39 by msamoile         ###   ########.fr       */
+/*   Updated: 2021/02/20 15:57:54 by msamoile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(char *str);
+int	ft_atoi(char *str)
 {
-	
+	int i;
+	int result;
+	int sign;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+			|| str[i] == '\f' || str[i] == '\r')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result * sign);
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	char str[] = "- 12345";
+	int val = ft_atoi(str);
+	printf("%d", val);
+	return (0);
 }
